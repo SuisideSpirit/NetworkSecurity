bc8b50beeb1308ae84bd20383371bb625edc1853
SuisideSpirit <pratiksinghnegi7@gmail.com>
2026-07-15 02:30:49 +0530

    Model Trainer , mlflow and Evaluation

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.logger import logging
 from networksecurity.exceptions.exception import NetworkSecurityException
-from networksecurity.entity.config_entity import DataIngestionConfig , DataValidationConfig ,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig , DataValidationConfig ,DataTransformationConfig , ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
-
-
+import mlflow 
 if __name__ == "__main__":
     try:
+        
         trainingpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(training_pipeline_config=trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
@@ -27,6 +28,12 @@ if __name__ == "__main__":
         data_transformation_artifact = data_transformation.initiate_data_tranformation()
         print(data_transformation_artifact)
         logging.info("Data Tranformation completed")
+
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config=trainingpipelineconfig)
+        model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact , model_trainer_config= model_trainer_config)
+        model_trainer.initiate_model_trainer() 
+        logging.info("Model Trainer artifact created")
+        
     except Exception as e : 
         raise NetworkSecurityException(e,sys)
         
